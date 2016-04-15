435195c5539e7ef431caf99cc7f87d86831eecff
John Gu <johng@tune.com>
2016-04-15 15:00:48 -0700

    Initial commit

---
+++ Example/Segment-Tune/TuneAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TuneAppDelegate.h
+//  Segment-Tune
+//
+//  Created by John Gu on 04/15/2016.
+//  Copyright (c) 2016 John Gu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TuneAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
